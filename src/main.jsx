f957372ed844aae12262ee50282cbab9e9d1af74
Crhisanto76 <antonypoure2@gmail.com>
2023-07-03 11:52:53 +0200

    add editJsx, desstroyJsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,31 +3,47 @@ import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './error-page';
 import './index.css';
-import Contact, { loader as contactLoader } from './routes/contact';
+import Contact, { loader as contactLoader, action as contactAction, } from './routes/contact';
 import Root, {
     loader as rootLoader,
     action as rootAction,
+    action,
 } from './routes/root';
 import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from "./routes/destroy";
+import Index from './routes/index';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
-        errorElement: <ErrorPage />,
         loader: rootLoader,
         action: rootAction,
+        errorElement: <ErrorPage />,
         children: [
             {
-                path: 'contacts/:contactId',
-                element: <Contact />,
-                loader: contactLoader,
-            },
-            {
-                path: 'contacts/:contactId/edit',
-                element: <EditContact />,
-                loader: contactLoader,
-                action: editAction,
+                errorElement: <ErrorPage />,
+                children: [
+                    { index: true, element: <Index /> },
+                    {
+                        path: "contacts/:contactId",
+                        element: <Contact />,
+                        loader: contactLoader,
+                        action: contactAction,
+                    },
+                    {
+                        path: 'contacts/:contactId/edit',
+                        element: <EditContact />,
+                        loader: contactLoader,
+                        action: editAction,
+                        
+                    },
+                    {
+                        path: "contacts/:contactId/destroy",
+                        action: destroyAction,
+                        errorElement: <div>Oops! there was an error.</div>,
+                    },
+                ],
             },
         ],
     },
